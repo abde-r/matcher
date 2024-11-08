da1dbd5776cd786a5331c48006304dac2a264929
dependentmadani <madani.badaoui12@gmail.com>
2024-11-08 18:55:51 +0100

    update: start working on components and authentication

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,7 +1,11 @@
 // import Image from "next/image";
+import Navbar from "@/components/Navbar";
 
 export default function Home() {
   return (
-    <div>Welcome to MatcherX</div>
+    <div>
+      <Navbar />
+      Welcome to MatcherX
+    </div>
   );
 }
