30b8444c74a54d73f5117cfda7cffeee1e3cbb1d
dependentmadani <madani.badaoui12@gmail.com>
2024-11-09 16:04:46 +0100

    update: working on auth pages and components

---
+++ frontend/components/Auth/index.tsx
@@ -0,0 +1,11 @@
+'use client';
+
+import React, { useState } from 'react';
+
+export default function AuthComponent() {
+    const [isLogged, setIsLogged] = useState(false);
+    
+    return (
+        <div>login component</div>
+    );
+}
