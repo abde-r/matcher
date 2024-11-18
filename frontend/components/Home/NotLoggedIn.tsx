a7e7ca32e302b9bcbfa4cfdfa62a15394cd02624
dependentmadani <madani.badaoui12@gmail.com>
2024-11-18 15:24:45 +0100

    update (home not logged in): responsiveness is working good now

--- frontend/components/Home/NotLoggedIn.tsx
+++ frontend/components/Home/NotLoggedIn.tsx
@@ -17,15 +17,15 @@ export default function NotLoggedInHomeComponent() {
     }
 
     return (
-        <div className="flex h-screen bg-gray-300">
+        <div className="flex h-screen bg-gray-300 overflow-y-scroll">
             <div className="md:w-1/2 w-full flex flex-col items-center justify-center pl-8">
                 <div className="sm:text-8xl text-6xl space-y-10 w-full">
                     <h1 className="font-bold">MatcherX</h1>
                     <h2 className="font-light space-y-10">Where Sparks Fly and Connections Blossom in Every Like.</h2>
                 </div>
-                <div className="flex flex-col pt-40 pl-36 w-full sm:text-3xl text-xl">
-                    <h1 className="text-red-800 font-bold">Start licking by creating your account today for free ✨</h1>
-                    <button type="button" className="w-1/5 ml-auto text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800" onClick={getStartedButton}>
+                <div className="flex flex-col pt-40 md:pl-36 pl-12 w-full sm:text-3xl text-xl">
+                    <h1 className="text-red-800 font-bold">Start liking by creating your account today for free ✨</h1>
+                    <button type="button" className="md:w-1/3 w-1/2 ml-auto text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800" onClick={getStartedButton}>
                         Get started
                     </button>
                 </div>
