a49c50fbda2328e18d8ea83f207b45132dc17843
dependentmadani <madani.badaoui12@gmail.com>
2024-11-19 16:33:14 +0100

    update (about page): about page is working and responsive

--- frontend/app/about/page.tsx
+++ frontend/app/about/page.tsx
@@ -1,10 +1,12 @@
+import React from "react";
 import NotLoggedNavbar from "@/components/Navbar/NotLoggedIn";
+import AboutComponent from "@/components/About/About";
 
 export default function About() {
     return (
         <div className="justify-center items-center min-h-screen bg-gradient-to-r from-gray-800 to-gray-900">
             <NotLoggedNavbar />
-            About page
+            <AboutComponent />
         </div>
     );
 }
